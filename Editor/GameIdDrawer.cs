e36d8b62bf223d0f2a4631d248586442387cc736
coryleach <cory.leach@gmail.com>
2019-09-15 21:50:24 -0400

    GameId drawer

---
+++ Editor/GameIdDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Gameframe.InfoTables.Editor
+{
+  [CustomPropertyDrawer(typeof(GameId))]
+  public class IngredientDrawer : PropertyDrawer
+  {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+      EditorGUI.BeginProperty(position, label, property);
+      EditorGUI.PropertyField(position, property.FindPropertyRelative("key"), label);
+      EditorGUI.EndProperty();
+    }
+  }
+}
