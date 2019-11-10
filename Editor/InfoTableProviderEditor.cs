b821ed7843f7388f24c50b8fdb415298b817cd61
Cory Leach <cory.leach@gmail.com>
2019-11-10 00:44:23 -0500

    Info table provider
    Adding a quick and easy way to load and get all all your info tables.

---
+++ Editor/InfoTableProviderEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gameframe.InfoTables.Editor
+{
+    [CustomEditor(typeof(InfoTableProvider))]
+    public class InfoTableProviderEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Gather"))
+            {
+                ((InfoTableProvider)target).GatherEntries();
+            }
+        }
+    }
+}
+
---
+++ Runtime/InfoTableProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Gameframe.InfoTables
+{
+    [CreateAssetMenu(menuName = "Gameframe/InfoTableProvider")]
+    public class InfoTableProvider : ScriptableObject
+    {
+        [SerializeField]
+        private List<BaseInfoTable> tables = new List<BaseInfoTable>();
+
+        [NonSerialized]
+        private Dictionary<Type, List<BaseInfoTable>> _tableDictionary = new Dictionary<Type, List<BaseInfoTable>>();
+        
+        private void OnEnable()
+        {
+            for (int i = 0; i < tables.Count; i++)
+            {
+                var table = tables[i];
+                
+                if (table == null)
+                {
+                    continue;
+                }
+
+                var tableType = table.GetType();
+                
+                if (!_tableDictionary.TryGetValue(tableType, out var list))
+                {
+                    list = new List<BaseInfoTable>();
+                    _tableDictionary.Add(tableType,list);
+                }
+                
+                list.Add(table);
+            }
+        }
+        
+        public T Get<T>() where T : BaseInfoTable
+        {
+            if (!_tableDictionary.TryGetValue(typeof(T), out var val))
+            {
+                return null;
+            }
+            
+            if (val.Count == 1)
+            {
+                return (T)val[0];
+            }
+            
+            return null;
+        }
+        
+        public IEnumerable<T> GetAll<T>() where T : BaseInfoTable
+        {
+            if (!_tableDictionary.TryGetValue(typeof(T), out var val))
+            {
+                return null;
+            }
+            return val.Cast<T>();
+        }
+        
+        #if UNITY_EDITOR
+        
+        public void GatherEntries()
+        {
+            //Remove Null Entries
+            tables.RemoveAll((x) => x == null);
+      
+            //Find all assets of type T and add them to our list
+            var guids = UnityEditor.AssetDatabase.FindAssets($"t:{typeof(BaseInfoTable)}");
+            foreach (var guid in guids)
+            {
+                string assetPath = UnityEditor.AssetDatabase.GUIDToAssetPath( guid );
+                BaseInfoTable asset = UnityEditor.AssetDatabase.LoadAssetAtPath<BaseInfoTable>( assetPath );
+                if( asset != null && !tables.Contains(asset) )
+                {
+                    tables.Add(asset);
+                }
+            }
+        }
+        
+        #endif
+        
+    }
+}
+
