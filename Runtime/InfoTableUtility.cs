aef0eaa79f4ceefdc70a446419f3714a6938bfb9
coryleach <cory.leach@gmail.com>
2020-06-18 21:04:20 -0400

    InfoTableUtility

---
+++ Runtime/InfoTableUtility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gameframe.InfoTables
+{
+    public class InfoTableUtility : MonoBehaviour
+    {
+        public static string SanitizeStringForId(string key)
+        {
+            return key.ToLowerInvariant().Replace(' ', '_').Replace("\'", "");
+        }
+    
+        public static string SanitizeStringForEnum(string key)
+        {
+            return key.Replace(" ", "").Replace("\'", "");
+        }
+    }
+}
+
+
